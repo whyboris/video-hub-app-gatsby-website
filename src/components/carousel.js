663e920dfe5e4e32161d188304a63a3b619ff374
whyboris <yboris@yahoo.com>
2020-09-20 17:45:43 -0400

    many featuers

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,65 +1,120 @@
 import React from "react"
 
-import Swiper from 'react-id-swiper';
+import Swiper from "react-id-swiper"
 
-import './swiper.css'
+import "./swiper.css"
 
 import { injectIntl } from "gatsby-plugin-intl"
 
-import Step from "./step"
+import FeatureList from "./step"
 
-const Carousel = ({data}) => {
-
+const Carousel = ({ data }) => {
   const params = {
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    spaceBetween: 30
+    spaceBetween: 30,
   }
 
   return (
-
     <Swiper {...params}>
-
       <div>
-        <Step
+        <FeatureList
           data={{
-            img: '/images/1small.jpg',
-            features: ["features.feature1", "features.feature2"],
+            img: "/images/3small.jpg",
+            heading: "Search by:",
+            features: [
+              "Folder name",
+              "File name",
+              "Tag",
+              "Fuzzy search",
+              "Regex search",
+              "... and more"
+            ],
             mirror: false,
           }}
         />
       </div>
 
       <div>
-        <Step
+        <FeatureList
           data={{
-            img: '/images/2small.jpg',
-            features: ["features.feature2", "features.feature6"],
+            img: "/images/1small.jpg",
+            heading: "Sort by:",
+            features: [
+              "File size",
+              "Duration",
+              "Star rating",
+              "Date created",
+              "Date modified",
+              "... and more",
+            ],
             mirror: false,
           }}
         />
       </div>
 
       <div>
-        <Step
+        <FeatureList
           data={{
-            img: '/images/3small.jpg',
-            features: ["features.feature3", "features.feature6"],
+            img: "/images/1small.jpg",
+            heading: "Tags:",
+            features: [
+              "Add your own tags to videos",
+              "Batch tagging",
+              "Drag and drop to add tags",
+              "Tag auto-complete when adding or searching",
+              "Add year tag",
+              "Add any description text",
+            ],
             mirror: false,
           }}
         />
       </div>
 
+      <div>
+        <FeatureList
+          data={{
+            img: "/images/2small.jpg",
+            heading: "More:",
+            features: [
+              "Find duplicates",
+              "Rename files",
+              "Extract video clips",
+              "Folder navigation",
+              "Custom thumbnails",
+              "Open video from screenshot you click",
+            ],
+            mirror: false,
+          }}
+        />
+      </div>
+
+      <div>
+        <FeatureList
+          data={{
+            img: "/images/2small.jpg",
+            heading: "And more:",
+            features: [
+              "Translations into 16 languages",
+              "Touch bar support on Mac",
+              "Delete file option",
+              "Create playlist of all videos",
+              "Drag video into video editor",
+              "... and more"
+            ],
+            mirror: false,
+          }}
+        />
+      </div>
     </Swiper>
-
   )
 }
 
--- src/components/step.js
+++ src/components/step.js
@@ -2,26 +2,27 @@ import React from "react"
 
 import { injectIntl, FormattedMessage } from "gatsby-plugin-intl"
 
-const Step = ({data}) => {
-
+const FeatureList = ({ data }) => {
   return (
-
-    <div className="step" style={{ flexDirection: data.mirror ?  'row-reverse' : 'initial' }}>
+    <div
+      className="step"
+      style={{ flexDirection: data.mirror ? "row-reverse" : "initial" }}
+    >
       <div className="step-image">
-        <img src={data.img} alt={'feature ' + data.step} />
+        <img src={data.img} alt={"feature " + data.step} />
       </div>
       <div className="step-text">
+        {data.heading ? (
+          <span class="feature-heading">{data.heading}</span>
+        ) : null}
         <ul className="feature-list">
-          { data.features.map((feature) => {
-            return(
-              <li>{ feature }</li>
-            )
+          {data.features.map((feature) => {
+            return <li>{feature}</li>
           })}
         </ul>
       </div>
     </div>
-
   )
 }
 
-export default injectIntl(Step)
+export default injectIntl(FeatureList)
