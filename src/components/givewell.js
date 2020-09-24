4d3ed01b3878ded61dbcb0fef53e5bfca3395c0b
whyboris <yboris@yahoo.com>
2020-09-24 00:02:34 -0400

    GiveWell i18n

--- src/components/givewell.js
+++ src/components/givewell.js
@@ -4,9 +4,9 @@ import Swiper from "react-id-swiper"
 
 import "./swiper.css"
 
-import { injectIntl } from "gatsby-plugin-intl"
+import { FormattedMessage, injectIntl } from "gatsby-plugin-intl"
 
-const GiveWell = () => {
+const GiveWell = ({ intl }) => {
   const params = {
     loop: true,
     spaceBetween: 30,
@@ -21,42 +21,28 @@ const GiveWell = () => {
       <div className="quote-container give-well">
         <Swiper {...params}>
           <div className="pull-quote">
-            <span>
-              "Focus on a program with excellent evidence of effectiveness and
-              cost-effectiveness."
-            </span>
+            <span>{intl.formatMessage({ id: "givewell.q1" })}</span>
             <span className="quote-author">
               <a href="https://www.givewell.org/charities/amf">GiveWell</a>
             </span>
           </div>
 
           <div className="pull-quote">
-            <span>
-              "Processes for ensuring that nets reach their intended recipients
-              and monitoring whether they remain in homes and in good condition
-              over the long-term."
-            </span>
+            <span>{intl.formatMessage({ id: "givewell.q2" })}</span>
             <span className="quote-author">
               <a href="https://www.givewell.org/charities/amf">GiveWell</a>
             </span>
           </div>
 
           <div className="pull-quote">
-            <span>
-              "Room for more funding – we believe AMF will be able to use
-              additional funds to deliver additional nets."
-            </span>
+            <span>{intl.formatMessage({ id: "givewell.q3" })}</span>
             <span className="quote-author">
               <a href="https://www.givewell.org/charities/amf">GiveWell</a>
             </span>
           </div>
 
           <div className="pull-quote">
-            <span>
-              "Transparency – AMF shares significant information about its work
-              with us and we are able to closely follow and understand its
-              work."
-            </span>
+            <span>{intl.formatMessage({ id: "givewell.q4" })}</span>
             <span className="quote-author">
               <a href="https://www.givewell.org/charities/amf">GiveWell</a>
             </span>
