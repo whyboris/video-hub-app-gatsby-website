b2eb30d737c4b229aefffbde620eba46a99463f9
whyboris <yboris@yahoo.com>
2021-02-20 11:58:29 -0500

    thank you page after purchase

---
+++ src/pages/thankyou.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { FormattedMessage, injectIntl } from "gatsby-plugin-intl"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "../components/buycoffee.scss"
+
+const Share = ({ intl }) => (
+  <Layout>
+    <SEO
+      lang={intl.locale}
+      title={intl.formatMessage({ id: "thankyou.title" })}
+      description={intl.formatMessage({ id: "general.description" })}
+    />
+
+    <section className="please-share">
+      <h1>{intl.formatMessage({ id: "thankyou.title" })}</h1>
+
+      <p>{intl.formatMessage({ id: "thankyou.thankyoutext" })}</p>
+      <p>{intl.formatMessage({ id: "thankyou.linkdescription" })}</p>
+      <p>
+        <center>
+          <a href="https://my.videohubapp.com">my.videohubapp.com</a>
+        </center>
+      </p>
+    </section>
+  </Layout>
+)
+
+export default injectIntl(Share)
