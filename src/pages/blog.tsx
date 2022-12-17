99ea53b4d6070ffd06ae2d77fae24cdc42ebbf31
Boris Yakubchik <yboris@yahoo.com>
2022-12-16 19:31:00 -0500

    4,600th purchase

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -35,8 +35,8 @@ type CurrentView = "all" | "donations" | "releases"
 
 let currentlyShowing: CurrentView = "all"
 
-const TOTAL_DONATION_DOLLAR_AMOUNT: string = "15,750"
-const LAST_DONATION_DATE: string = "Oct 27, 2022"
+const TOTAL_DONATION_DOLLAR_AMOUNT: string = "16,100"
+const LAST_DONATION_DATE: string = "Dec 16, 2022"
 const LAST_RELEASE_DATE: string = "Feb 20, 2021"
 
 const Blog = ({ intl }) => {
@@ -175,6 +175,16 @@ const Blog = ({ intl }) => {
             ? intl.formatMessage({ id: "blog.donations" })
             : intl.formatMessage({ id: "blog.releases" })}
         </h1>
+
+        <Receipt
+          data={{
+            currentlyShowing,
+            amount: "4,600",
+            date: "Dec 16, 2022",
+            receipt:
+              "https://www.againstmalaria.com/MyNets.aspx?DonationID=1032196",
+          }}
+        />
 
         <Receipt
           data={{
